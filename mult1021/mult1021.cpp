c1d4d6ab449b49003ca8beee92e5bb2d4c2e16de
buffa <buffaloes0534@icloud.com>
2025-10-27 09:34:03 +0900

    mult

--- mult1021/mult1021.cpp
+++ mult1021/mult1021.cpp
@@ -3,11 +3,17 @@ using namespace std;
 
 static int mult(int x, int y)
 {
-
+    return x * y;
 }
 
 int main()
 {
-    
+    int x = 7;
+    int y = 3;
+    int answer = 0;
+
+    answer = mult(x, y);
+
+    cout << "答えは " << answer << endl;
 }
 
